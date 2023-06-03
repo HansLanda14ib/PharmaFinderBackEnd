3e3a6d4f199a7dc4a5b52a595c0bf8c71d0aee0c
Hansa Landa <bibzazne@gmail.com>
2023-06-04 00:55:57 +0100

    https://pharmafinder-app-front-end.vercel.app/

--- src/main/java/com/localisation/security/config/SecurityConfiguration.java
+++ src/main/java/com/localisation/security/config/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Explicitly allow localhost:3000
+        configuration.setAllowedOrigins(Arrays.asList("https://pharmafinder-app-front-end.vercel.app/")); // Explicitly allow localhost:3000
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
